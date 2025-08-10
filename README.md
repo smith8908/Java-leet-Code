58356224457ca96dfe73a1b37debe415c82dbf15
Smith Rouniyar <130716901+smith8908@users.noreply.github.com>
2025-08-10 16:59:40 +0530

    Update and rename README.md to next-greater-element-I.java

--- README.md
+++
@@ -1,1 +0,0 @@
-# Java-leet-Code
---
+++ next-greater-element-I.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int num : nums2) {
+            while (!stack.isEmpty() && num > stack.peek()) {
+                nextGreater.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        while (!stack.isEmpty()) {
+            nextGreater.put(stack.pop(), -1);
+        }
+        
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreater.get(nums1[i]);
+        }
+        return result;
+    }
+}
